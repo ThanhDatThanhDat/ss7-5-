ddef3ac52242229e1e5f71def9bb9591129855ed
ThanhDatThanhDat <dt2229992@gmail.com>
2024-11-28 08:13:32 +0700

    ss7-5

---
+++ ss7-5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    int arr[5] = {1,2,3,4,5};
+	int i;
+	for(i = 0;i< 5; i++){
+		printf("%d \n", arr[i]);
+    }
+    
+    int max = arr[0];
+    int min = arr[0]; 
+
+    for (i = 1; i < 5; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+     
+    printf("Phan tu lon nhat trong mang la: %d\n", max);
+    printf("Phan tu nho nhat trong mang la: %d\n", min);
+
+    
+	return 0;
+}
